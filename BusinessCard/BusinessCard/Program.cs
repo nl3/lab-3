462bbcf6304d488b6b87e569cede3f83e8a8af5c
VasylynaTsyhan <vasylynatsyhan93@outlook.com>
2016-05-24 22:35:58 +0300

    merge BusinesCard

---
+++ BusinessCard/BusinessCard/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessCard
+{
+    class BusinessClient
+    {
+        public string newclient = "";
+        public static string Client(string name, string sername, int balanse, string card)
+        {
+            name = "Cat";
+            sername = "Kothkuj";  //Miay Miay
+            balanse = 100000;
+            
+            return card =name + sername;
+
+        }
+    
+    }
+}
