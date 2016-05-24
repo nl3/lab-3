7cac61725d118b0697cae024d3d79293f0489fd8
Ostashevska <fistashka116@gmail.com>
2016-05-24 22:18:52 +0300

    we have a new client

---
+++ BusinessCard/BusinessCard/Class1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessCard
+{
+    class Class1
+    {
+       public string name = "Cat";
+       public string sername = "Kothkuj";  //Miay Miay
+       public  int balanse = 100000;
+    }
+}
