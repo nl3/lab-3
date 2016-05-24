77674c363e34ebdd207fb79d0a35ac1c6f15f906
VasylynaTsyhan <vasylynatsyhan93@outlook.com>
2016-05-24 21:34:55 +0300

    class Purse

---
+++ lab3/lab3/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab3
+{
+    class Purse
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
